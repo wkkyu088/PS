1193a10e4c4876128869591e091d7fc59057bd1a
wkkyu088 <dnjsrbwls08@gmail.com>
2022-07-14 20:25:42 +0900

    0714

---
+++ Programmers/Python/자연수 뒤집어 배열로 만들기.py
@@ -0,0 +1,5 @@
+def solution(n):
+    return list(map(int, reversed(str(n))))
+
+def solution(n):
+    return list(map(int, str(n)))[::-1]
--- Programmers/Python/자연수뒤집어배열로만들기.py
+++
@@ -1,2 +0,0 @@
-def solution(n):
-    return list(map(int, str(n)))[::-1]
