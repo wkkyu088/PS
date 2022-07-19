5282b8f0f6e6d673e12a9fcf6a9519f370771bdb
wkkyu088 <dnjsrbwls08@gmail.com>
2022-07-19 21:30:56 +0900

    0719

--- Programmers/Python/수박수박수박수박수박수.py
+++ Programmers/Python/수박수박수박수박수박수.py
@@ -1,8 +1,2 @@
 def solution(n):
-    answer = ''
-    for i in range(1, n+1):
-        if i%2 == 1:
-            answer += '수'
-        else:
-            answer += '박'
-    return answer
+    return "수박"*(n//2)+"수"*(n%2)
